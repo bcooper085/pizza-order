f79b39f4851865bbdb23a936cadb7d837ce7ea70
brandon <bcooper085@gmail.com>
2017-01-27 14:21:55 -0800

    add pizza toppings to array, add array to price prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,44 @@
 //Business Logic
-var price = 0;
-
-function Pizza(size, toppings) {
+function Pizza(size) {
   this.pizzaSize = size;
-  this.pizzaToppings = toppings;
+  this.pizzaToppings = [];
 }
 
-Pizza.prototype.order = function() {
-  return this.pizzaSize + " " + this.pizzaToppings;
-}
-
-var toppingCost = function(toppings) {
-  for (var i = 0; i < toppings.length; i++) {
+Pizza.prototype.price = function() {
+  var price = 0;
+  if (this.pizzaSize === "Small") {
+    price += 10;
+  } else if (this.pizzaSize === "Medium") {
+    price += 13;
+  } else {
+    price += 15;
+  }
+  for (var i = 0; i < this.pizzaToppings.length; i++) {
     price++;
   }
   return price;
+}
+
+Pizza.prototype.order = function() {
+  return (this.pizzaSize + " " + this.pizzaToppings);
 }
 
 //User Logic
 $(function() {
   $('.order-form').submit(function() {
     event.preventDefault();
-    var inputtedSize = $('input[name=pizzaSize]:checked').val();
-    var inputtedToppings = $('input[name=pizzaToppings]:checked').val();
-    var newPizza = new Pizza(inputtedSize, inputtedToppings);
 
-    $('#output-order').text(newPizza.order());
+    var inputtedSize = $('input[name=pizzaSize]:checked').val();
+    var newPizza = new Pizza(inputtedSize);
+
+    $('input[name=pizzaToppings]:checked').each(function() {
+      var inputtedToppings = $(this).val();
+      newPizza.pizzaToppings.push(inputtedToppings);
+    });
+
+    var orderCost = newPizza.price();
+
+    $('#output-order').text(newPizza.order() + orderCost);
 
     // $('form#order-form-toppings').removeAttr('checked');
     // $('input[name="pizzaToppings"]:checked').removeAttr('checked');
