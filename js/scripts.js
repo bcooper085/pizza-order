f0d9f941c51d28fe2b4ea5b1e51c4a06605caec7
brandon <bcooper085@gmail.com>
2017-01-27 12:25:37 -0800

    bootstrap applied to forms for betteer visual testing

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,35 @@
 //Business Logic
 var price = 0;
 
-function Pizza(size,toppings) {
+function Pizza(size, toppings) {
   this.pizzaSize = size;
   this.pizzaToppings = toppings;
 }
 
-Pizza.prototype.Order = function() {
+Pizza.prototype.order = function() {
   return this.pizzaSize + " " + this.pizzaToppings;
+}
+
+var toppingCost = function(toppings) {
+  for (var i = 0; i < toppings.length; i++) {
+    price++;
+  }
+  return price;
 }
 
 //User Logic
 $(function() {
+  $('#order-form-toppings').submit(function() {
+    event.preventDefault();
+    var inputtedSize = $('input[name=pizzaSize]:checked').val();
+    var inputtedToppings = $('input[name=pizzaToppings]:checked').val();
 
+    var newPizza = new Pizza(inputtedSize, inputtedToppings);
+
+    $('#output-order').append(newPizza.order());
+
+    $('input[name=pizzaSize]').removeAttr('checked');
+    $('input[name=pizzaToppings]').removeAttr('checked');
+
+  });
 });
